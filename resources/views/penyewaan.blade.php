07642cefe8deb783696a05ed9fd42337fb036380
asmajati15 <asmajati.cooler15@gmail.com>
2022-01-16 01:05:46 +0700

    first

---
+++ resources/views/penyewaan.blade.php
@@ -0,0 +1,100 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="/css/style.css">
+
+    <title>Salakanagara Bus</title>
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
+        <div class="container-fluid">
+            <a class="navbar-brand" id="namapo" href="/"><i>SALAKANAGARA</i></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="/">Beranda</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/class">Kelas Armada</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/rent">Penyewaan</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/about">Tentang Kami</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container text-center py-5">
+        <h2 id="namapo" class="pb-2"><i>SALAKANAGARA</i></h2>
+        <h4>Untuk Penyewaan armada bisa menghubungi nomor di Footer</h4>
+    </div>
+
+    <section id="address" class="address mb-5">
+        <div class="container">
+            <div class="row pt-5">
+                <div class="col">
+                    <h2>Contact us</h2>
+                </div>
+            </div>
+            <div class="row mt-5">
+                <div class="col">
+                    <h4>Telfon</h4>
+                    <p>0838-7715-8482<br>0851-5784-8662</p>
+                    <h4>Instagram</h4>
+                    <p>@abisatya_group</p>
+                </div>
+                <div class="col">
+                    <h4>Kantor Pusat</h4>
+                    <p>Jl. Kol. Ahmad Syam, Katulampa, Kota Bogor</p>
+                    <h4>Kantor Cabang</h4>
+                    <p>Ngadiluwih, Kota Kediri</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <footer class="bg-dark text-white">
+        <div class="container">
+            <div class="row pt-2">
+                <div class="col text-center">
+                    <p>Asmajati Ananto | 2021</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <div class="container">
+
+    </div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
